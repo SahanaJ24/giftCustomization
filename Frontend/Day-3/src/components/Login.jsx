5dc0f9643cca1930b7d043e95a60dea8013a2a97
SahanaJ24 <jsahana2003@gmail.com>
2024-01-31 14:18:11 +0530

    Day 3

---
+++ Frontend/Day-3/src/components/Login.jsx
@@ -0,0 +1,24 @@
+import React from 'react'//rafce
+import '../assets/css/login.css'
+const Login = () => {
+  return (
+    
+      <div className='login'>
+        <div className='login-container'>
+            <h1>Sign In</h1>
+            {/* <div className='login-fields'>
+                <input type='text' placeholder='Your name'/><br/><br/>
+                <input type='email' placeholder='Email Address'/><br/><br/>
+                <input type='password' placeholder='Password'/>
+            </div>
+            
+            <button>Continue</button>
+            <p className='login-existing'>Already have an account?<span>Login</span></p>
+            <div className='login-agree'> */}
+            {/* </div> */}
+        </div>
+  </div>
+  )
+}
+
+export default Login
